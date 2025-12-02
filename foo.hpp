bb7e6cfe82f39b9dbfb810d84bbcb6b07299696f
lziolek <lubomirziolek>
2025-12-02 15:55:08 +0100

    test

--- foo.hpp
+++ foo.hpp
@@ -9,8 +9,9 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
-
     // Twoja implementacja tutaj
+    size_t i=0;
+    std::for_each(people.rbegin(),people.rend(),[&](Human& p) {p.birthday(); retval[i++]=(p.isMonster()? 'n' : 'y');});
+       return retval;
 
-    return retval;
 }
